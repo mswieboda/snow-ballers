cae13442d5e2466f3d81b207c7b6bf2e5d9b8e87
Matt Swieboda <mswiebod@gmail.com>
2017-02-20 18:10:41 -0600

    Removes splats if more than max

---
+++ Assets/Scripts/SplattedSnowballController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplattedSnowballController : MonoBehaviour {
+
+	public int maxSplats = 100;
+
+	private static List<GameObject> splats = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		splats.Add(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (splats.Count > maxSplats) {
+			destroyASplat();
+		}
+	}
+
+	static void destroyASplat() {
+		GameObject splat = splats[0];
+		splats.RemoveAt(0);
+		Destroy(splat);
+	}
+}
