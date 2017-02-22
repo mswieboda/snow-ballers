1aac635fa5a7d11d11381dd68fe3321b64ef562b
Ben Eng <whysguy52@gmail.com>
2017-02-22 00:05:01 -0600

    Cursor is locked to the screen

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class MenuScript : MonoBehaviour {
 
+	//CursorLockMode mode;
+
 	// Use this for initialization
 	void Start () {
-		
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//TODO: THIS IS TEMPORARY AND SHOULD BE MOVED TO A BUTTON
 		if (Input.GetKey("escape")) {
 			Application.Quit();
 		}
