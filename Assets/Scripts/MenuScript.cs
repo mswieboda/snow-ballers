f4b51af4cc257d3e79fb9135dcfbfed9ecc1be60
Matt Swieboda <mswiebod@gmail.com>
2017-02-26 19:42:29 -0600

    Adds main menu scene

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,21 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
-
-	//CursorLockMode mode;
-
-	// Use this for initialization
 	void Start () {
 		Cursor.lockState = CursorLockMode.Locked;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		//TODO: THIS IS TEMPORARY AND SHOULD BE MOVED TO A BUTTON
-		if (Input.GetKey("escape")) {
-			Application.Quit();
+		if (Input.GetButtonDown("Menu")) {
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.Locked;
+			SceneManager.LoadScene(0);
 		}
 	}
 }
